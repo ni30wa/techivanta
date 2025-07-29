7c842f6263abe0c0662737b1c8635896dea08598
Nitish Kumar <stnitish01@gmail.com>
2025-07-30 00:22:00 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -60,7 +60,7 @@ app.get("/api", (req, res) => {
 // ✅ Serve frontend (React build)
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
