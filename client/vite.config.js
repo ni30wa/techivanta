c847e8e0edb85b3f12842af818019ba73239e41f
Nitish Kumar <stnitish01@gmail.com>
2025-07-30 00:14:36 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
@@ -16,9 +17,14 @@ export default defineConfig(({ mode }) => {
       },
     },
     build: {
-      outDir: "dist",
-      base: "/",
-      chunkSizeWarningLimit: 1000, // ✅ warning disappears
+      outDir: "dist",       
+      base: "./",          
+      chunkSizeWarningLimit: 1000,
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
     },
   };
 });
